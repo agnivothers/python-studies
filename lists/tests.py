5946d3bf2943f5703572c0e5170055fabb30c584
Agniv <agniv.others@gmail.com>
2019-08-18 01:03:49 +0530

    Front page HTML now generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 class SmokeTest(TestCase):
   def test_bad_maths(self):
@@ -11,11 +12,15 @@ class HomePageTest(TestCase):
   def test_root_url_resolves_to_home_page(self):
     found = resolve('/')
     self.assertEqual(found.func, home_page)
-  
-  def test_home_page_returns_correct_html(self):
+
+  def test_home_page_returns_correct_html_old(self):
     request = HttpRequest()
     response = home_page(request)
     html = response.content.decode('utf8')
     self.assertTrue(html.startswith('<html>'))
     self.assertIn('<title>To-Do lists</title>',html)
-    self.assertTrue(html.endswith('</html>'))
+    self.assertTrue(html.strip().endswith('</html>'))
+
+  def test_home_page_returns_correct_html(self):
+    response = self.client.get('/')
+    self.assertTemplateUsed(response,'home.html')
--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-  return HttpResponse('<html><title>To-Do lists</title></html>')
+  #return HttpResponse('<html><title>To-Do lists</title><head><h1>To-Do</h1><head></html>')
+  return render(request, 'home.html')
